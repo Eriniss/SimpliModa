bcf5e543cb6c15c5d81124c8fd56f60f178eb975
Eriniss <jhs2195@gmail.com>
2023-09-28 12:21:18 +0900

    remove unnecessary resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+import Navbar from './components/public/Navbar';
+
+const Mainbody = styled.div`
+  color: #ffffff;
+  background-color: #222222;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Navbar />
       </header>
-    </div>
+      <body>body</body>
+      <footer>footer</footer>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/public/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavbarBody = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 80px;
+  color: #ffffff;
+  background-color: #121212;
+`;
+
+const Navbar = () => {
+  return <NavbarBody></NavbarBody>;
+};
+
+export default Navbar;
